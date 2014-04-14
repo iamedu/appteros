a834f06249852336c3bcc3117856cc9738894d44
Eduardo Díaz <iamedu@gmail.com>
2014-04-14 01:47:40 -0500

    Improving build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ fi
 cd _build
 cmake ..
 make -j2
-install_name_tool -change "@executable_path/Chromium Embedded Framework" "../Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework" appteros
+install_name_tool -change "@executable_path/Chromium Embedded Framework" "@executable_path/../Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework" appteros
 
 cd ..
 
@@ -17,4 +17,7 @@ mkdir -p Appteros.app/Contents/{Frameworks,MacOS,Resources}
 cp -r ../Frameworks/Chromium\ Embedded\ Framework.framework Appteros.app/Contents/Frameworks
 cp -r ../Frameworks/libplugin_carbon_interpose.dylib Appteros.app/Contents/Frameworks
 
-cp mac/Info.plist Appteros.app/Contents
+cp mac/PkgInfo mac/Info.plist Appteros.app/Contents
+cp -r mac/appteros.icns mac/English.lproj Appteros.app/Contents/Resources
+
+cp _build/appteros Appteros.app/Contents/MacOS
